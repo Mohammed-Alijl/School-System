257939b02695ac13e896420a004ee37014867be6
Mohammed-Alijl <moh.wa.2016@gmail.com>
2026-01-31 19:38:01 +0200

    feat(dashboard): setup valex dashboard

--- routes/admin.php
+++ routes/admin.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:admin', 'throttle:admin'])->group(function () {
+Route::middleware(['throttle:admin'])->group(function () {
 
+    Route::get('/', function () { return view('admin.index');});
+    Route::get("/{page}", [DashboardController::class, "index"])->name("admin.page");
 });
