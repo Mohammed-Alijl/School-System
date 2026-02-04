6f615cd77180dbad91d3f4a13298d2ad2eb361a2
Mohammed-Alijl <moh.wa.2016@gmail.com>
2026-02-04 16:35:00 +0200

    feat: use the mcamara package in admin routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,8 +14,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
         then: function () {
             Route::middleware('web')
-                ->prefix("admin")
-                ->name('admin.')
                 ->group(base_path('routes/admin.php'));
 
         }
@@ -37,6 +35,11 @@ return Application::configure(basePath: dirname(__DIR__))
 
         $middleware->alias([
             'admin.verified' => EmailIsVerified::class,
+            'localize'                => \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationRoutes::class,
+            'localizationRedirect'    => \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationRedirectFilter::class,
+            'localeSessionRedirect'   => \Mcamara\LaravelLocalization\Middleware\LocaleSessionRedirect::class,
+            'localeCookieRedirect'    => \Mcamara\LaravelLocalization\Middleware\LocaleCookieRedirect::class,
+            'localeViewPath'          => \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationViewPath::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
--- routes/admin.php
+++ routes/admin.php
@@ -5,43 +5,42 @@ use App\Http\Controllers\Admin\Auth\NewPasswordController;
 use App\Http\Controllers\Admin\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Admin\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
-Route::middleware(['guest:admin','throttle:auth-attempts'])->group(function () {
-    Route::get('login', [AdminAuthController::class, 'create'])->name('login');
-    Route::post('login', [AdminAuthController::class, 'store'])->name('login.store');
-    Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
-        ->name('password.request');
+Route::group(
+    [
+        'prefix' => LaravelLocalization::setLocale(),
+        'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath' ]
+    ], function(){
 
-    Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])
-        ->name('password.email');
+    Route::prefix('admin')->name('admin.')->group(function() {
 
-    Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])
-        ->name('password.reset');
+        Route::middleware(['guest:admin','throttle:auth-attempts'])->group(function () {
+            Route::get('login', [AdminAuthController::class, 'create'])->name('login');
+            Route::post('login', [AdminAuthController::class, 'store'])->name('login.store');
 
-    Route::post('reset-password', [NewPasswordController::class, 'store'])
-        ->name('password.store');
-});
+            Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])->name('password.request');
+            Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])->name('password.email');
+            Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])->name('password.reset');
+            Route::post('reset-password', [NewPasswordController::class, 'store'])->name('password.store');
+        });
 
-Route::middleware('auth:admin')->group(function () {
+        Route::middleware('auth:admin')->group(function () {
+            Route::get('verify-email', [VerifyEmailController::class, '__invoke'])->name('verification.notice');
+            Route::post('email/verification-notification', [VerifyEmailController::class, 'store'])
+                ->middleware('throttle:6,1')->name('verification.send');
+            Route::get('verify-email/{id}/{hash}', [VerifyEmailController::class, 'verify'])
+                ->middleware(['signed', 'throttle:6,1'])->name('verification.verify');
+        });
 
-    Route::get('verify-email', [VerifyEmailController::class, '__invoke'])
-        ->name('verification.notice');
+        Route::middleware(['auth:admin','throttle:admin'])->group(function () {
 
-    Route::post('email/verification-notification', [VerifyEmailController::class, 'store'])
-        ->middleware('throttle:6,1')
-        ->name('verification.send');
+            Route::middleware(['admin.verified'])->group(function () {
+                Route::get('/', function () { return view('admin.index');})->name('dashboard');
+            });
 
-    Route::get('verify-email/{id}/{hash}', [VerifyEmailController::class, 'verify'])
-        ->middleware(['signed', 'throttle:6,1'])
-        ->name('verification.verify');
+            Route::post('logout', [AdminAuthController::class, 'destroy'])->name('logout');
+        });
 
-});
-
-Route::middleware(['auth:admin','throttle:admin'])->group(function () {
-
-    Route::middleware(['admin.verified'])->group(function () {
-        Route::get('/', function () { return view('admin.index');})->name('dashboard');
     });
-
-    Route::post('logout', [AdminAuthController::class, 'destroy'])->name('logout');
 });
