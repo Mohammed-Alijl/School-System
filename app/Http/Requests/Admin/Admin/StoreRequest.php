7b98d96c02dc20968353d5a0bc84feb4adec4d70
Mohammed-Alijl <moh.wa.2016@gmail.com>
2026-02-07 11:15:31 +0200

    feat: create store admin request file

---
+++ app/Http/Requests/Admin/Admin/StoreRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\Admin\Admin;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:30|min:3|unique:admins,name',
+            'email' => 'required|email|unique:admins,email|max:50|min:3',
+            'password' => 'required|confirmed|min:8|max:30',
+            'roles_name' => 'required',
+            'image' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ];
+    }
+}
