c2791a508a2bb43def556cb4e12902f5466c14a4
Mohammed-Alijl <moh.wa.2016@gmail.com>
2026-02-19 18:59:53 +0200

    fix: use right tables names of classrooms and type bloods

--- database/migrations/2026_02_19_151622_create_students_table.php
+++ database/migrations/2026_02_19_151622_create_students_table.php
@@ -20,11 +20,11 @@ return new class extends Migration
             $table->string('national_id')->unique();
             $table->date('date_of_birth');
             $table->foreignId('grade_id')->constrained('grades')->cascadeOnUpdate()->restrictOnDelete();
-            $table->foreignId('classroom_id')->constrained('classrooms')->cascadeOnUpdate()->restrictOnDelete();
+            $table->foreignId('classroom_id')->constrained('class_rooms')->cascadeOnUpdate()->restrictOnDelete();
             $table->foreignId('section_id')->constrained('sections')->cascadeOnUpdate()->restrictOnDelete();
             $table->string('academic_year');
             $table->foreignId('guardian_id')->constrained('guardians')->cascadeOnUpdate()->restrictOnDelete();
-            $table->foreignId('blood_type_id')->constrained('blood_types')->cascadeOnUpdate()->restrictOnDelete();
+            $table->foreignId('blood_type_id')->constrained('type_bloods')->cascadeOnUpdate()->restrictOnDelete();
             $table->foreignId('nationality_id')->constrained('nationalities')->cascadeOnUpdate()->restrictOnDelete();
             $table->foreignId('religion_id')->constrained('religions')->cascadeOnUpdate()->restrictOnDelete();
             $table->foreignId('gender_id')->constrained('genders')->cascadeOnUpdate()->restrictOnDelete();
