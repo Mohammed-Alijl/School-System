bdb0816d48062be9618c2462e519e18c4de5b1d3
Mohammed-Alijl <moh.wa.2016@gmail.com>
2026-03-10 20:59:43 +0200

    feat(finance): setup dynamic fee categories and fees schema

---
+++ database/migrations/2026_03_10_184144_create_fees_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('fees', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('title');
+
+            $table->decimal('amount', 8, 2);
+            $table->foreignId('fee_category_id')->constrained('fee_categories')->restrictOnDelete();
+
+            $table->foreignId('academic_year_id')->constrained('academic_years')->cascadeOnDelete();
+            $table->foreignId('grade_id')->constrained('grades')->cascadeOnDelete();
+            $table->foreignId('classroom_id')->nullable()->constrained('classrooms')->cascadeOnDelete();
+
+            $table->text('description')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('fees');
+    }
+};
